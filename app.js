8272b3c87b019f0faae341b653c4272dc1c27f36
ishuah91 <ishuah91@gmail.com>
2013-12-31 22:03:49 +0300

    test 3: public test

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var url = require("url");
 var app = express();
+//total objects updated
+var objects = 0;
 
 app.use(logfmt.requestLogger());
+app.use(express.bodyParser());
 
 //home
 app.get('/', function(req, res) {
-  res.send('Received');
+  res.send('Total objects: '+objects);
 });
 
 //subscriptions post
 app.post('/callback', function(req, res){
-	res.send('Post');
+	var obj = req.body;
+	console.log("obj: "+obj.length);
+	objects += obj.length
+	res.send(200);
 });
 
-//subscriptions get
+//subscriptions get; part of the  Pubsubhubub challenge flow.
 app.get('/callback', function(req, res){
-	res.send('Get');
+	var parsedUrl = url.parse(req.url, true);
+	var urlObjects = parsedUrl.query;
+	res.send(urlObjects["hub.challenge"]);
 });
 
 var port = process.env.PORT || 5000;
