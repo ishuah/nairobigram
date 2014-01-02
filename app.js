33eacafb3ceca71d4986f1093c779adc5fdb3628
ishuah91 <ishuah91@gmail.com>
2014-01-02 19:16:55 +0300

    added routes, views and public folder

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var url = require("url");
+var io = require("socket.io");
 var app = express();
+var routes = require('./routes');
+var path = require('path');
+var http = require('http');
 //total objects updated
-var objects = 0;
+var objects;
 
-app.use(logfmt.requestLogger());
-app.use(express.bodyParser());
+app.configure(function(){
+  app.set('port', process.env.PORT || 8080);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.bodyParser());
+  app.use(logfmt.requestLogger());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
+var sio = io.listen(server);
+
+sio.sockets.on('connection', function (socket) {
+	socket.emit('message', { message: 'Connected' });
+});
 
 //home
-app.get('/', function(req, res) {
-  res.send('Total objects: '+objects);
+app.get('/', function(req, res){
+	res.send(objects);
 });
 
 //subscriptions post
 app.post('/callback', function(req, res){
 	var obj = req.body;
-	console.log("obj: "+obj.length);
-	objects += obj.length
+	objects = obj;
 	res.send(200);
 });
 
@@ -26,9 +53,4 @@ app.get('/callback', function(req, res){
 	var parsedUrl = url.parse(req.url, true);
 	var urlObjects = parsedUrl.query;
 	res.send(urlObjects["hub.challenge"]);
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
 });
